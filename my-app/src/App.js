31d9707cd7535d1dc60de0117a2f36b48ebec65a
Nicolas Grosso <nicolas.grosso@watea.com.ar>
2020-12-15 12:22:01 -0300

    Clase 44, se agrega la primer impresion de react hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Zephi", age: 25 }
+    ],
+    otherState: "Some other value"
+  }
+
+  switchNameHandler = () => {
+    //console.log("Was Clicked");
+    //NO USAR!!, react no va a permitir la mutacion del estado diretamente this.state.persons[0].name = "Maximilian"
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Zephi", age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies are: Racing</Person>
-        <Person name="Zephi" age="27"/>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies are: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.CreateElement('div',{className:'App'},React.CreateElement('h1',null, 'I\'m a React App!!!'));
